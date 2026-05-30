2142f5626176652dad241f54b2a27c635fddaf61
jedidot <jehoonl@gmail.com>
2026-05-30 16:11:41 +0900

    Update - 구매처 페이지 무한루핑 오류

--- next.config.ts
+++ next.config.ts
@@ -5,15 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  async redirects() {
-    return [
-      {
-        source: "/where-to-buy",
-        destination: "/where-to-buy/",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
